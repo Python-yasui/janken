62e05829481ee24dccf7520479634f3ca1d058b5
Python-yasui <yasui.ryuhei@gmail.com>
2019-01-02 16:49:36 +0900

    新しいじゃんけ加

--- janken.py
+++ janken.py
@@ -1,13 +1,25 @@
+# # -*- coding: utf-8 -*-
+
+# import random
+# data = ["goo","choki","pa"]
+# data_choice = random.choice(data)
+# print(data_choice)
+
+# print('入力してください')
+# input = raw_input()
+
+# # print('あなたの入力した文字は%x', %input, 'です')
+
+# print('あなたの入力した文字は「' + input + '」です')
+
 # -*- coding: utf-8 -*-
 
 import random
-data = ["goo","choki","pa"]
-data_choice = random.choice(data)
-print(data_choice)
+dic = {'a': 'グー','b': 'チョキ','c':'パー'}
+print('じゃんけん')
+print('a=グー　b=チョキ c=パー　aかbかcを入力')
+user = input('>>>   ')
+user = user.lower()
 
-print('入力してください')
-input = raw_input()
+try:
 
-# print('あなたの入力した文字は%x', %input, 'です')
-
-print('あなたの入力した文字は「' + input + '」です')
