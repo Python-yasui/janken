76443d00f0b035dc47af59d77502ea33a833c027
Python-yasui <yasui.ryuhei@gmail.com>
2019-01-02 12:17:06 +0900

    first commit

---
+++ janken.py
@@ -0,0 +1,9 @@
+import random
+data = ["goo","choki","pa"]
+data_choice = random.choice(data)
+print(data_choice)
+
+# import calender
+# import calender,math
+# cal = calender.TextCalender()
+# cal.prmonh(2018,2)
