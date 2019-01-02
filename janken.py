592a6f8322f5adfd44ffe817b7975bab9eb2bc23
Python-yasui <yasui.ryuhei@gmail.com>
2019-01-02 15:28:11 +0900

    コメントアウト追加

--- janken.py
+++ janken.py
@@ -3,7 +3,7 @@ data = ["goo","choki","pa"]
 data_choice = random.choice(data)
 print(data_choice)
 
-import calender
-import calender,math
-cal = calender.TextCalender()
-cal.prmonh(2018,2)
+# import calender
+# import calender,math
+# cal = calender.TextCalender()
+# cal.prmonh(2018,2)
