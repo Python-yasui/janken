9e2a3070e52e4165a5d78dafedafdcfe4cfc6ebb
Python-yasui <yasui.ryuhei@gmail.com>
2019-01-02 17:05:11 +0900

    新しいじゃnn

--- janken.py
+++ janken.py
@@ -23,3 +23,38 @@ user = user.lower()
 
 try:
 
+user_choice = dic[user]
+
+choice_list = ['a','b','c']
+pc = dic[random.choice(choice_list)]
+
+draw = 'DRAW'
+win = 'You Win!!!'
+lose = 'You Lose!!!'
+
+if user-choise ==pc:
+    judge = draw
+else:
+    if user_choice =='グー'
+    　　　if pc =='チョキ'：
+            judge = win
+    else:
+    judge = lose
+
+elif user_choice == 'チョキ':
+　　if pc == 'パー'：
+　　　　judge = win
+  else:
+       judge = lose
+else:
+    if pc =='グー':
+        judge = win
+    else:
+        judge = lose
+
+print('あなたの選んだのは　％s'　％ suer_choice)
+print('コンピュータが選んだのは　％s'　％　pc)
+print('結果は％s'　％　judge)
+
+except:
+    print('aかbかcを入力してください。')　　
