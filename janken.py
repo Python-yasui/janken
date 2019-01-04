6133acc4a854f8671b00cc259446f003e9b12df6
Python-yasui <yasui.ryuhei@gmail.com>
2019-01-05 02:23:44 +0900

    同じファイルで勉強させて

--- janken.py
+++ janken.py
@@ -67,7 +67,47 @@
 # life = today - birthday
 # print(life.days)
 
-print(30 * 365)
+import datetime
+date_a = datetime.date(2008,1,1)
+date_b = datetime.date(2009,1,1)
+days_2008 = date_b -date_a
+print(days_2008)
+
+import datetime
+date_a = datetime.date(2015,1,1)
+date_b = datetime.date(2016,1,1)
+days_2015 = date_b -date_a
+print(days_2015)
+
+import datetime
+date_a = datetime.date(2016,1,1)
+date_b = datetime.date(2017,1,1)
+days_2016 = date_b -date_a
+print(days_2016)
+
+import datetime
+date_a = datetime.date(2017,1,1)
+date_b = datetime.date(2018,1,1)
+days_2017 = date_b -date_a
+print(days_2017)
+
+import datetime
+date_a = datetime.date(2018,1,1)
+date_b = datetime.date(2019,1,1)
+days_2018 = date_b -date_a
+print(days_2018)
+
+import datetime
+date_a = datetime.date(2019,1,1)
+date_b = datetime.date(2020,1,1)
+days_2019 = date_b -date_a
+print(days_2019)
+
+import datetime
+date_a = datetime.date(2020,1,1)
+date_b = datetime.date(2021,1,1)
+days_2020 = date_b -date_a
+print(days_2020)
 
 
 
