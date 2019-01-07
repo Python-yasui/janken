f45a37f9f96aa5e5a2012c6d71abf82969642d18
Python-yasui <yasui.ryuhei@gmail.com>
2019-01-07 18:51:44 +0900

    組み合わせがむずいね

--- janken.py
+++ janken.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# import random
-# data = ["goo","choki","pa"]
-# data_choice = random.choice(data)
-# print(data_choice)
+import random
+data = ["goo","choki","pa"]
+data_choice = random.choice(data)
+print(data_choice)
 
-# print('入力してください')
-# input = raw_input()
+print('入力してください')
+input = raw_input()
 
-# # print('あなたの入力した文字は%x', %input, 'です')
+# print('あなたの入力した文字は%x', %input, 'です')
 
 # print('あなたの入力した文字は「' + input + '」です')
 
@@ -129,13 +129,17 @@
 #     print(val)
 
 
-list_tohoku = [5349,5478,5344,4644,4968,6259]
-list_shikoku = [3148,2991,2966,2457]
-avg_tohoku = 0
+# list_tohoku = [5349,5478,5344,4644,4968,6259]
+# list_shikoku = [3148,2991,2966,2457]
+# avg_tohoku = 0
 
-for val in list_tohoku:
-    avg_tohoku += val
+# for val in list_tohoku:
+#     avg_tohoku += val
 
-avg_tohoku /= len(list_tohoku)
-print(avg_tohoku)
+# avg_tohoku /= len(list_tohoku)
+# print(avg_tohoku)
 
+
+# score = 100
+# if score == 100:
+#     print('君は天才だ')
